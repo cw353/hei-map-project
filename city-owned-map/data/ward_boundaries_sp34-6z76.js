1e98d523cd1a39bff1645bec783a1acef6ffd007
cw353 <claire@Claires-Air.attlocal.net>
2022-06-20 11:37:13 -0400

    updated map description

--- city-owned-map/data/ward_boundaries_sp34-6z76.js
+++ city-owned-map/data/ward_boundaries_sp34-6z76.js
@@ -1,4 +1,7 @@
 /* Data source: https://data.cityofchicago.org/Facilities-Geographic-Boundaries/Boundaries-Wards-2015-/sp34-6z76. This dataset, which contains ward boundaries from May 2015 onwards, was accessed on 14 June 2022, at which time it had last been updated on 23 Aug. 2016. */
+
+const wardBoundariesAccessTime = "14 June 2022";
+
 const wardBoundaries = {
   "type": "FeatureCollection",
   "features": [
