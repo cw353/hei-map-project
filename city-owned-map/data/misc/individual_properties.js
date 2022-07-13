e1fecc8255b1d298b75cbafe9f29a739dc0c7f06
cw353 <claire@Claires-Air.attlocal.net>
2022-07-13 13:07:07 -0400

    Updated data and data generation for individual properties

--- city-owned-map/data/misc/individual_properties.js
+++ city-owned-map/data/misc/individual_properties.js
@@ -1,5 +1,5 @@
-const sunshine_gospel = {"pin":20154030220000.0,"longitude":-87.6128976931,"latitude":41.7841023762};
+const sunshine_gospel = {"pin":20154030220000,"property_address":"500 E 61ST ST","longitude":-87.6128976931,"latitude":41.7841023762};
 
 const ward_20_office = {"ward":20,"alderman":"Taylor, Jeanette B.","address":"5707 South Wentworth Avenue","latitude":"41.7905","longitude":"-87.630388"};
 
-const ward_offices_metadata = {"id": "htai-wnw4", "name": "Ward Offices", "description": "List of alderman, office address, phone number, and website for all 50 Chicago wards.", "dataUri": "https://data.cityofchicago.org/resource/htai-wnw4", "attribution": "City of Chicago", "attributionLink": "http://www.chicago.gov", "actualDatasourceUri": null, "createdAt": "22 Dec 2010", "dataUpdatedAt": "06 Jun 2022", "metadataUpdatedAt": "06 Jun 2022", "accessedOn": "09 Jul 2022", "dataUseNotes": "Used to obtain location information for the Ward 20 office."};
+const ward_offices_metadata = {"id": "htai-wnw4", "name": "Ward Offices", "description": "List of alderman, office address, phone number, and website for all 50 Chicago wards.", "dataUri": "https://data.cityofchicago.org/resource/htai-wnw4", "attribution": "City of Chicago", "attributionLink": "http://www.chicago.gov", "actualDatasourceUri": null, "createdAt": "22 Dec 2010", "dataUpdatedAt": "06 Jun 2022", "metadataUpdatedAt": "06 Jun 2022", "accessedOn": "13 Jul 2022", "dataUseNotes": "Used to obtain location information for the Ward 20 office."};
