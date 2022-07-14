5fd8671cb8bbe35e4556162b334899572bb5375a
cw353 <claire@Claires-Air.attlocal.net>
2022-07-14 12:51:32 -0400

    updated datagroups to accommodate data generation changes; updated marker popup size options

--- city-owned-map/code/map_classes.js
+++ city-owned-map/code/map_classes.js
@@ -120,7 +120,7 @@ class Datagroup extends ChildLayerGroup {
     this.#setMarkerData(identifier, markerData);
     marker.setIcon(this.getMarkerIcon(markerData));
     if (this.getMarkerPopupContent) {
-      marker.bindPopup("", { maxWidth: 250, maxHeight: 300, });
+      marker.bindPopup("", { maxWidth: 300, minWidth: 250, maxHeight: 300, });
       this.#setMarkerPopupContent(markerData);
     }
   }
